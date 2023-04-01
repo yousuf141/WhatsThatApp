1d0ac7eac337ced456cf135c725148b0a362177b
yousuf141 <77011760+yousuf141@users.noreply.github.com>
2023-04-01 06:48:05 +0100

    create ui for login section

--- components/sections/auth/LoginSection.tsx
+++ components/sections/auth/LoginSection.tsx
@@ -1,15 +1,83 @@
 import React from "react";
 import { View } from "react-native";
-import { Button } from "react-native-paper";
+
+import { Button, HelperText, Snackbar, TextInput } from "react-native-paper";
+
+import { validateEmail, validatePassword } from "../../../utils/validators";
 
 const LoginSection: React.FC<{ navigation: any }> = ({ navigation }) => {
+  const [email, setEmail] = React.useState<string>("");
+  const isEmailValid = validateEmail(email);
+
+  const [password, setPassword] = React.useState<string>("");
+  const isPasswordValid = validatePassword(password);
+
+  const [snackbarVisible, setSnackbarVisible] = React.useState<boolean>(false);
+  const [snackbarMessage, setSnackbarMessage] = React.useState<string>("");
+
+  function handleLogin() {
+    if (!isEmailValid || !isPasswordValid) {
+      showSnackbar("Please make sure all fields are valid.");
+      return;
+    }
+
+    // TODO: implement actual login
+  }
+
   function handleGoToSignUp() {
     navigation.navigate("SignUpSection");
   }
 
+  function showSnackbar(message: string, time: number = 1500) {
+    setSnackbarMessage(message);
+    setSnackbarVisible(true);
+    setTimeout(() => setSnackbarVisible(false), time);
+  }
+
   return (
-    <View>
-      <Button onPress={handleGoToSignUp}>Go to Sign Up</Button>
+    <View style={{ flex: 1, justifyContent: "space-between" }}>
+      <View>
+        <View>
+          <TextInput
+            label="Email"
+            left={<TextInput.Icon icon={"email"} />}
+            value={email}
+            onChangeText={setEmail}
+            error={!isEmailValid && email.length > 0}
+          />
+          <HelperText type="error" visible={!isEmailValid && email.length > 0}>
+            Email is invalid
+          </HelperText>
+        </View>
+        <View>
+          <TextInput
+            label="Password"
+            left={<TextInput.Icon icon={"lock"} />}
+            value={password}
+            secureTextEntry
+            onChangeText={setPassword}
+            error={!isPasswordValid && password.length > 0}
+          />
+          <HelperText
+            type="error"
+            visible={!isPasswordValid && password.length > 0}
+          >
+            Password is invalid
+          </HelperText>
+        </View>
+      </View>
+      <View>
+        <Button mode="contained" onPress={handleLogin}>
+          Log In
+        </Button>
+        <Button onPress={handleGoToSignUp}>Sign Up</Button>
+      </View>
+      <Snackbar
+        visible={snackbarVisible}
+        onDismiss={() => setSnackbarVisible(false)}
+      >
+        {snackbarMessage}
+      </Snackbar>
     </View>
   );
 };
---
+++ utils/validators.ts
@@ -0,0 +1,9 @@
+export const validateEmail = (email: string) => {
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailRegex.test(email);
+};
+
+export const validatePassword = (password: string) => {
+  const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$/;
+  return passwordRegex.test(password);
+};
