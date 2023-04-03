3c301847e30f6e9f1a98ffa9372a3774f3c944b1
yousuf141 <77011760+yousuf141@users.noreply.github.com>
2023-04-03 08:57:31 +0100

    create function definitions for profile actions

--- components/profile/ProfileActions.tsx
+++ components/profile/ProfileActions.tsx
@@ -7,6 +7,14 @@ import { useAuth } from "../../providers/AuthProvider";
 const ProfileActions: React.FC = () => {
   const [, authDispatch] = useAuth();
 
+  function handleUpdateInfo(): void {
+    // TODO: implement updating of users info. another screen needed?
+  }
+
+  function handleUpdatePhoto(): void {
+    // TODO: see how native image picking can be done using expo
+  }
+
   function handleLogout(): void {
     authDispatch({ userId: undefined, key: undefined });
   }
@@ -14,18 +22,26 @@ const ProfileActions: React.FC = () => {
   return (
     <View style={{ flex: 1, justifyContent: "space-between" }}>
       <View>
-        <Button style={{ margin: 5 }} mode="contained">
+        <Button
+          style={{ margin: 5 }}
+          mode="contained"
+          onPress={handleUpdateInfo}
+        >
           Update Info
         </Button>
-        <Button style={{ margin: 5 }} mode="outlined">
+        <Button
+          style={{ margin: 5 }}
+          mode="outlined"
+          onPress={handleUpdatePhoto}
+        >
           Update Photo
         </Button>
       </View>
       <View>
         <Button
           style={{ margin: 5 }}
-          onPress={handleLogout}
           mode="contained-tonal"
+          onPress={handleLogout}
         >
           Log Out
         </Button>
