64ac11a4cb6014c504c816f0345319ab58a38446
yousuf141 <77011760+yousuf141@users.noreply.github.com>
2023-04-04 21:20:50 +0100

    update screens to contain stacks

--- screens/ChatsScreen.tsx
+++ screens/ChatsScreen.tsx
@@ -1,8 +1,27 @@
 import React from "react";
-import { Text } from "react-native-paper";
+
+import { createStackNavigator } from "@react-navigation/stack";
+
+import ChatsSection from "../sections/chat/ChatsSection";
+import ChatSection from "../sections/chat/ChatSection";
+
+const Stack = createStackNavigator();
 
 const ChatsScreen: React.FC = () => {
-  return <Text>Chats Screen</Text>;
+  return (
+    <Stack.Navigator initialRouteName="ChatsSection">
+      <Stack.Screen
+        options={{ headerTitle: "Chats" }}
+        name="ChatsSection"
+        component={ChatsSection}
+      />
+      <Stack.Screen
+        options={{ headerTitle: "Chat" }}
+        name="ChatSection"
+        component={ChatSection}
+      />
+    </Stack.Navigator>
+  );
 };
 
 export default ChatsScreen;
--- screens/ContactsScreen.tsx
+++ screens/ContactsScreen.tsx
@@ -1,8 +1,27 @@
 import React from "react";
-import { Text } from "react-native-paper";
+
+import { createStackNavigator } from "@react-navigation/stack";
+
+import ContactsSection from "../sections/contact/ContactsSection";
+import ContactSection from "../sections/contact/ContactSection";
+
+const Stack = createStackNavigator();
 
 const ContactsScreen: React.FC = () => {
-  return <Text>Contacts Screen</Text>;
+  return (
+    <Stack.Navigator initialRouteName="ContactsSection">
+      <Stack.Screen
+        options={{ headerTitle: "Contacts" }}
+        name="ContactsSection"
+        component={ContactsSection}
+      />
+      <Stack.Screen
+        options={{ headerTitle: "Contact" }}
+        name="ContactSection"
+        component={ContactSection}
+      />
+    </Stack.Navigator>
+  );
 };
 
 export default ContactsScreen;
